3f3f2d48505e7f233287289e93ed3694e7a551b2
kko0831 <kko0831@hanmail.net>
2020-10-31 02:41:54 +0900

    try different method for running tests(Getting the Jest tests to run in Parallel - Part 28)

--- src/modules/register/register.test.ts
+++ src/modules/register/register.test.ts
@@ -1,4 +1,5 @@
-// import { request } from "graphql-request";
+import { Connection } from "typeorm";
+import * as faker from "faker";
 import { User } from "../../entity/User";
 import {
   duplicateEmail,
@@ -6,17 +7,18 @@ import {
   invalidEmail,
   passwordNotLongEnough,
 } from "./errorMessages";
-import { createTypeormConn } from "../../utils/createTypeormConn";
-import { Connection } from "typeorm";
 import { TestClient } from "../../utils/TestClient";
+import { createTestConn } from "../../testUtils/createTestConn";
 
-const email = "tom@bob.com";
-const password = "jalksdf";
+const email = faker.internet.email();
+const password = faker.internet.password();
+
+const client = new TestClient(process.env.TEST_HOST as string);
 
 let conn: Connection;
 
 beforeAll(async () => {
-  conn = await createTypeormConn();
+  conn = await createTestConn();
 });
 
 afterAll(async () => {
@@ -25,7 +27,6 @@ afterAll(async () => {
 
 describe("Register user", () => {
   it("check for duplicate emails", async () => {
-    const client = new TestClient(process.env.TEST_HOST as string);
     // make sure we can register a user
     const response = await client.register(email, password);
     expect(response.data).toEqual({ register: null });
@@ -44,7 +45,6 @@ describe("Register user", () => {
   });
 
   it("check bad email", async () => {
-    const client = new TestClient(process.env.TEST_HOST as string);
     const response3 = await client.register("b", password);
     expect(response3.data).toEqual({
       register: [
@@ -62,8 +62,7 @@ describe("Register user", () => {
 
   it("check bad password", async () => {
     // catch bad password
-    const client = new TestClient(process.env.TEST_HOST as string);
-    const response4 = await client.register(email, "ad");
+    const response4 = await client.register(faker.internet.email(), "ad");
     expect(response4.data).toEqual({
       register: [
         {
@@ -75,7 +74,6 @@ describe("Register user", () => {
   });
 
   it("check bad password and bad email", async () => {
-    const client = new TestClient(process.env.TEST_HOST as string);
     const response5 = await client.register("df", "ad");
     expect(response5.data).toEqual({
       register: [
